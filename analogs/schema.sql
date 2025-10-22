e2ddfbbd438fe72c71e3b4839ac8b0b21ccf72d2
Samuel Heinrich <shazamuel89@gmail.com>
2025-10-21 21:20:24 -0500

    *Initialize project structure with templates, database schema, static files, and README.*

---
+++ analogs/schema.sql
@@ -0,0 +1,30 @@
+DROP TABLE IF EXISTS comment;
+DROP TABLE IF EXISTS analysis;
+DROP TABLE IF EXISTS user;
+
+
+CREATE TABLE user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+);
+
+CREATE TABLE analysis (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    author_id INTEGER NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    song_title TEXT NOT NULL,
+    artist TEXT NOT NULL,
+    analysis_body TEXT NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES user (id),
+);
+
+CREATE TABLE comment (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    analysis_id INTEGER NOT NULL,
+    author_id INTEGER NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    content TEXT NOT NULL,
+    FOREIGN KEY (analysis_id) REFERENCES analysis (id),
+    FOREIGN KEY (author_id) REFERENCES user (id),
+);
